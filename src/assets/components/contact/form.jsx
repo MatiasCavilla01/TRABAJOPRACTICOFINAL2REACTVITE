7ca1bc54164ce1d6ba96be5240667e513f5c6d55
MatiasCavilla01 <Matias.cavilla20038@outlook.com>
2024-12-18 17:18:40 -0300

    primeros detalles resposive

--- src/assets/components/contact/form.jsx
+++ src/assets/components/contact/form.jsx
@@ -36,6 +36,96 @@ const ContactForm = () => {
 
   return (
     <Box
+      p={6}
+      shadow="md"
+      borderWidth="1px"
+      borderRadius="md"
+      color="#fb4b50"
+      width={{ base: '73%', md: '60%', lg: '36%' }}
+      mx="auto"
+      my={8}
+    >
+      <Heading as="h2" size="lg" mb={6} color="#fb4b50" textAlign="center">
+        Formulario de Contacto
+      </Heading>
+      <Formik
+        initialValues={{
+          nombre: '',
+          apellido: '',
+          email: '',
+          descripcion: '',
+        }}
+        validationSchema={ContactSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          emailjs
+            .send(
+              'service_yvny5pd',
+              'template_e2wrola',
+              {
+                user_name: `${values.nombre} ${values.apellido}`,
+                user_email: values.email,
+                user_descripcion: values.descripcion,
+              },
+              'cylz02dKL_9ocsXFM'
+            )
+            .then((response) => {
+              console.log('Mensaje enviado:', response.status, response.text);
+              setClicked(true);
+              setSubmitting(false);
+            })
+            .catch((err) => {
+              console.log('Error al enviar mensaje:', err);
+              setSubmitting(false);
+            });
+        }}
+      >
+        {({ isSubmitting, errors, touched }) => (
+          <Form>
+            <Flex direction="column" gap={4}>
+              <FormControl isInvalid={touched.nombre && errors.nombre}>
+                <Field as={Input} type="text" name="nombre" placeholder="Nombre" />
+                <FormErrorMessage>{errors.nombre}</FormErrorMessage>
+              </FormControl>
+
+              <FormControl isInvalid={touched.apellido && errors.apellido}>
+                <Field as={Input} type="text" name="apellido" placeholder="Apellido" />
+                <FormErrorMessage>{errors.apellido}</FormErrorMessage>
+              </FormControl>
+
+              <FormControl isInvalid={touched.email && errors.email}>
+                <Field as={Input} type="email" name="email" placeholder="Email" />
+                <FormErrorMessage>{errors.email}</FormErrorMessage>
+              </FormControl>
+
+              <FormControl isInvalid={touched.descripcion && errors.descripcion}>
+                <Field as={Textarea} name="descripcion" placeholder="Motivo de contacto" />
+                <FormErrorMessage>{errors.descripcion}</FormErrorMessage>
+              </FormControl>
+            </Flex>
+
+            <Button
+              mt={6}
+              width="100%"
+              type="submit"
+              colorScheme={clicked ? 'green' : 'blue'}
+              isLoading={isSubmitting}
+              transition="all 0.3s ease-in-out"
+            >
+              {clicked ? 'Enviado' : 'Enviar'}
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Box>
+  );
+};
+
+
+{/*const ContactForm = () => {
+  const [clicked, setClicked] = useState(false);
+
+  return (
+    <Box
       p={4}
       shadow="md"
       borderWidth="1px"
@@ -120,112 +210,21 @@ const ContactForm = () => {
               </FormControl>
             </Flex>
             <Button 
-              marginLeft="33%"
+              marginLeft="36%"
               type="submit"
               colorScheme={clicked ? 'green' : 'blue'} // Cambia el color después del clic
               isLoading={isSubmitting}
               transition="all 0.3s ease-in-out"
             >
-              {clicked ? 'Enviado' : 'Enviar'} {/* Cambia el texto después del clic */}
+              {clicked ? 'Enviado' : 'Enviar'}  Cambia el texto después del clic 
             </Button>
           </Form>
         )}
       </Formik>
     </Box>
   );
-};
+};*/}
 
-/* const ContactForm = () => (
-  
-  <Box p={4} shadow="md" borderWidth="1px" borderRadius="md" color='#fb4b50' width='36%' marginTop='5%' marginRight='3%' marginBottom='5%'  >
-    <Heading as="h2" size="lg" mb={4} color='#fb4b50'>
-      Formulario de Contacto
-    </Heading>
-    <Formik
-      initialValues={{
-        nombre: '',
-        apellido: '',
-        email: '',
-        descripcion: '',
-      }}
-      validationSchema={ContactSchema}
-      onSubmit={(values, { setSubmitting }) => {
-        emailjs.send(
-          'service_yvny5pd',
-          'template_e2wrola',
-          {
-            user_name: `${values.nombre} ${values.apellido}`,
-            user_email: values.email,
-            user_descripcion: values.descripcion
-          },
-          'cylz02dKL_9ocsXFM'
-        ).then((response) => {
-          console.log('Mensaje enviado:', response.status, response.text);
-          setSubmitting(false);
-        }).catch((err) => {
-          console.log('Error al enviar mensaje:', err);
-          setSubmitting(false);
-        });
-      }}
-    >
-      {({ isSubmitting, errors, touched }) => (
-        <Form>
-          <Flex direction="column" mb={4}>
-            <FormControl isInvalid={touched.nombre && errors.nombre}>
-              <Field
-                as={Input}
-                type="text"
-                name="nombre"
-                placeholder="Nombre"
-              />
-              {touched.nombre && errors.nombre && (
-                <FormErrorMessage>{errors.nombre}</FormErrorMessage>
-              )}
-            </FormControl>
-            <FormControl isInvalid={touched.apellido && errors.apellido}>
-              <Field
-                as={Input}
-                type="text"
-                name="apellido"
-                placeholder="Apellido"
-                marginTop="5%"
 
-              />
-              {touched.apellido && errors.apellido && (
-                <FormErrorMessage>{errors.apellido}</FormErrorMessage>
-              )}
-            </FormControl>
-            <FormControl isInvalid={touched.email && errors.email}>
-              <Field
-                as={Input}
-                type="email"
-                name="email"
-                placeholder="Email"
-                marginTop="5%"
-
-              />
-              {touched.email && errors.email && (
-                <FormErrorMessage>{errors.email}</FormErrorMessage>
-              )}
-            </FormControl>
-            <FormControl isInvalid={touched.descripcion && errors.descripcion}>
-              <Field
-                as={Textarea}
-                name="descripcion"
-                placeholder="Motivo de contacto"
-                marginTop="5%"
-
-              />
-              {touched.descripcion && errors.descripcion && (
-                <FormErrorMessage>{errors.descripcion}</FormErrorMessage>
-              )}
-            </FormControl>
-          </Flex>
-          <Button type='submit' colorScheme={clicked ? 'red' : 'green'} isLoading={isSubmitting}>Enviar</Button>
-        </Form>
-      )}
-    </Formik>
-  </Box>
-);*/
 
 export default ContactForm;
